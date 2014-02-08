9d3d94edf080b4d51c84e1077d58bc386455fd8f
Qian Zhang <qianz424@gmail.com>
2014-02-08 13:58:20 -0600

    Changed project name.

---
+++ FacialModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.FaceTracking;
+
+namespace FaceTrackingBasics
+{
+    class FacialModel
+    {
+        private EnumIndexableCollection<FeaturePoint, Vector3DF> features;
+        
+        public FacialModel(EnumIndexableCollection<FeaturePoint, Vector3DF> features)
+        {
+            
+        }
+
+        public FacialModel(string file)
+        {
+
+        }
+
+        public void Write(string file)
+        {
+
+        }
+
+
+
+    }
+}
